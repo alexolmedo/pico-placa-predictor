098ab1e7d4667775e8be6e03347b81eacac5e538
Alexander Olmedo <olmedo2408@gmail.com>
2020-09-02 23:45:33 -0500

    Day of week rules

--- scripts.js
+++ scripts.js
@@ -1,33 +1,45 @@
 function handleClick () {
 
-  //Check valid license
+  // Check valid license
   var licensePlateString = $('#licensePlateNumber').val()
   if (!licensePlateIsValid(licensePlateString)) {
     alert('The license plate is invalid')
     return
   }
 
-  var dateTimeRoadString = $('#dateTimeRoad').val()
-  var dateTimeRoad = moment(dateTimeRoadString, moment.DATETIME_LOCAL)
-  if (!dateTimeRoad.isValid()) {
+  // Check valid time
+  var dateTimeString = $('#dateTime').val()
+  var dateTime = moment(dateTimeString, moment.DATETIME_LOCAL)
+  if (!dateTime.isValid()) {
     alert('The date and time are invalid')
     return
   }
 
-  var lastdigit = licensePlateString[licensePlateString.length - 1]
-  console.log(hasPicoPlaca(lastdigit, dateTimeRoad))
+  var lastdigit = parseInt(licensePlateString[licensePlateString.length - 1])
+  console.log(hasPicoPlaca(lastdigit, dateTime))
 
 }
 
-const hasPicoPlaca = (lastDigit, dateTimeRoad) => {
-  var timeOnly = moment(dateTimeRoad.format('HH:mm'), 'HH:mm')
+const hasPicoPlaca = (lastDigit, dateTime) => {
+  var timeOnly = moment(dateTime.format('HH:mm'), 'HH:mm')
   var morningStart = moment('07:00', 'HH:mm')
   var morningEnd = moment('09:30', 'HH:mm')
   var afternoonStart = moment('16:00', 'HH:mm')
   var afternoonEnd = moment('19:30', 'HH:mm')
 
+  var rules = {
+    'Monday': [1, 2],
+    'Tuesday': [3, 4],
+    'Wednesday': [5, 6],
+    'Thursday': [7, 8],
+    'Friday': [9, 0],
+    'Saturday': [],
+    'Sunday': []
+  }
+
   if (timeOnly.isBetween(morningStart, morningEnd) || timeOnly.isBetween(afternoonStart, afternoonEnd)) {
-    return true
+    var dayOfWeek = dateTime.format('dddd')
+    return rules[dayOfWeek].indexOf(lastDigit) >= 0
   } else {
     return false
   }
@@ -35,5 +47,5 @@ const hasPicoPlaca = (lastDigit, dateTimeRoad) => {
 
 const licensePlateIsValid = (licensePlateString) => {
   // Check if plates are in format ABC-1234 or AB-1234, case insensitive
-  return licensePlateString.match(/^[A-Za-z]{3}-[1-9]{4}$|^[A-Za-z]{2}-[1-9]{4}$/)
+  return licensePlateString.match(/^[A-Za-z]{3}-[0-9]{4}$|^[A-Za-z]{2}-[0-9]{4}$/)
 }
